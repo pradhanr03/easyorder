1882ca0e676c86edca35a6df0afe92908655f003
Rubina Pradhan <pradhanr03@gmail.com>
2015-10-12 11:32:24 -0400

    mongo added

---
+++ routes/orders.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var restrict = require('../auth/restrict');
+
+/* GET home page. */
+router.get('/', restrict, function(req, res, next) {
+
+    var vm = {
+        title: 'Place an order',
+        orderId: req.session.orderId,
+        firstName: req.user ? req.user.firstName : null
+    }
+    res.render('orders/index', vm);
+});
+
+module.exports = router;
